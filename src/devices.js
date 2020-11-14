55c61adffe2dd34713ab9348f59d1f16d8a8af17
Rozhin Alex <alexey.rozhin90@gmail.com>
2020-11-15 01:31:41 +0300

    final: Создание элементов, улучшение верстки эмулятора

--- src/devices.js
+++ src/devices.js
@@ -1,14 +1,43 @@
 import React from "react";
-import { Button, ButtonGroup, Card, Elevation } from "@blueprintjs/core";
+import { Button, ButtonGroup } from "@blueprintjs/core";
 
-export const Devices = () => (
-  <Card interactive={true} elevation={Elevation.TWO}>
-    <ButtonGroup minimal={false} vertical>
-      <h2>Выбор устройств</h2>
-      <Button icon="full-circle">РЗА</Button>
-      <Button icon="full-circle">Промышленные коммутаторы</Button>
-      <Button icon="full-circle">.......</Button>
-      <Button icon="full-circle">Подключения</Button>
-    </ButtonGroup>
-  </Card>
-);
+const Devices = ({ onCreate }) => {
+  return (
+    <div className="devices-menu">
+      <h3 style={{ marginBottom: "20px" }}>Выбор устройств</h3>
+      <ButtonGroup large vertical>
+        <h5>Мехатроника</h5>
+        <ButtonGroup
+          vertical
+          style={{ marginTop: "4px", marginBottom: "4px", marginLeft: "4px" }}
+        >
+          <Button onClick={() => onCreate("БМРЗ 150")}>БМРЗ 150</Button>
+        </ButtonGroup>
+
+        <h5>Промышленные коммутаторы</h5>
+        <ButtonGroup
+          vertical
+          style={{ marginTop: "4px", marginBottom: "4px", marginLeft: "4px" }}
+        >
+          <Button onClick={() => onCreate("GL 100")}>GL 100</Button>
+          <Button onClick={() => onCreate("CISCO 1000")}>CISCO 1000</Button>
+        </ButtonGroup>
+
+        <h5>Провода</h5>
+        <ButtonGroup
+          vertical
+          style={{ marginTop: "4px", marginBottom: "4px", marginLeft: "4px" }}
+        >
+          <Button onClick={() => onCreate("Медный кабель")}>
+            Медный кабель
+          </Button>
+          <Button onClick={() => onCreate("Витая пара RJ45")}>
+            Витая пара RJ45
+          </Button>
+        </ButtonGroup>
+      </ButtonGroup>
+    </div>
+  );
+};
+
+export default Devices;
--- src/emulator/index.js
+++ src/emulator/index.js
@@ -1,83 +1,154 @@
-import React, { useRef, useEffect } from "react";
+import React, {Fragment, useRef, useEffect, useState} from "react";
 import { dia, shapes, ui } from "@clientio/rappid";
-import { Devices } from "../devices";
+import DevicesMenu from "../devices";
+
+const initCanvas = (canvas) => {
+  const graph = new dia.Graph();
+  const paper = new dia.Paper({
+    model: graph,
+    width: "100%",
+    height: "100vh",
+    interactive: { linkMove: false },
+    defaultConnectionPoint: {
+      name: "boundary",
+      args: {
+        extrapolate: true,
+        sticky: true,
+      },
+    },
+    validateConnection: function () {
+      return false;
+    },
+    background: {
+      color: "#F8F9FA",
+    },
+  });
+
+  const stencil = new ui.Stencil({ paper, width: 100, height: 100 });
+
+  const scroller = new ui.PaperScroller({
+    paper,
+    cursor: "grab",
+  });
+
+  canvas.current.appendChild(scroller.el);
+  canvas.current.appendChild(stencil.el);
+  scroller.render().center();
+  stencil.render();
+  return graph;
+};
+
+const createAddCommutator = (graph) => (x, y) => {
+  const commutator = new shapes.standard.Rectangle({
+    position: { x, y },
+    size: { width: 200, height: 50 },
+    attrs: {
+      label: {
+        text: "Коммутатор",
+      },
+    },
+  });
+  commutator.addTo(graph);
+  return commutator;
+};
+const createAddBMRZ = (graph) => (x, y) => {
+  const bmrz = new shapes.standard.Rectangle({
+    position: { x, y },
+    size: { width: 300, height: 300 },
+    attrs: {
+      label: {
+        text: "Механотроника БМРЗ",
+      },
+    },
+  });
+  bmrz.addTo(graph);
+  return bmrz;
+};
+const createConnectViaRJ45 = (graph) => (block1, block2) => {
+  const wire = new shapes.standard.ShadowLink({
+    smooth: true,
+    markup: [
+      {
+        tagName: "path",
+        selector: "shadow",
+        attributes: {
+          fill: "none",
+        },
+      },
+      {
+        tagName: "path",
+        selector: "line",
+        attributes: {
+          fill: "none",
+        },
+      },
+      {
+        tagName: "text",
+        selector: "label",
+      },
+    ],
+    attrs: {
+      line: {
+        stroke: "#aaa",
+      },
+      label: {
+        textPath: {
+          selector: "line",
+          startOffset: "50%",
+        },
+        textAnchor: "middle",
+        textVerticalAnchor: "middle",
+        text: "RJ45",
+        fill: "#f6f6f6",
+        fontSize: 12,
+        fontWeight: "bold",
+      },
+    },
+  });
+  wire.source(block1);
+  wire.target(block2);
+  wire.addTo(graph);
+  return wire;
+};
 
 const Emulator = () => {
   const canvas = useRef(null);
+  const [wire, setWire] = useState('Медный кабель')
+  let addBMRZ, addCommutator, connectViaRJ45;
 
   useEffect(() => {
-    const graph = new dia.Graph();
+    const graph = initCanvas(canvas);
+    addCommutator = createAddCommutator(graph);
+    addBMRZ = createAddBMRZ(graph);
+    connectViaRJ45 = createConnectViaRJ45(graph);
 
-    const paper = new dia.Paper({
-      model: graph,
-      width: "100%",
-      height: "100vh",
-      interactive: { linkMove: false },
-      defaultConnectionPoint: {
-        name: "boundary",
-        args: {
-          extrapolate: true,
-          sticky: true,
-        },
-      },
-      validateConnection: function () {
-        return false;
-      },
-      background: {
-        color: "#F8F9FA",
-      },
-    });
-
-    const scroller = new ui.PaperScroller({
-      paper,
-      cursor: "grab",
-    });
-
-    canvas.current.appendChild(scroller.el);
-    scroller.render().center();
-
-    const block1 = new shapes.standard.Rectangle({
-      position: { x: 100, y: 100 },
-      size: { width: 100, height: 50 },
-      attrs: {
-        label: {
-          text: "Блок АЦП",
-        },
-      },
-    });
-    const block2 = new shapes.standard.Rectangle({
-      position: { x: 100, y: 100 },
-      size: { width: 100, height: 50 },
-      attrs: {
-        label: {
-          text: "Блок АЦП",
-        },
-      },
-    });
-    const commutator = new shapes.standard.Rectangle({
-      position: { x: 300, y: 300 },
-      size: { width: 200, height: 50 },
-      attrs: {
-        label: {
-          text: "Коммутатор1",
-        },
-      },
-    });
-    const link = new shapes.standard.Link();
-    link.source(commutator);
-    link.target(block1);
-
-    commutator.addTo(graph);
-    block1.addTo(graph);
-    block2.addTo(graph);
-    link.addTo(graph);
+    // const block1 = addBMRZ(100, 100);
+    // const block2 = addBMRZ(100, 100);
+    // const commutator = addCommutator(100, 100);
+    //
+    // connectViaRJ45(commutator, block1);
+    // connectViaRJ45(commutator, block2);
   }, []);
 
   return (
-    <div>
-      <Devices />
+    <Fragment>
+      <DevicesMenu onCreate={(type) => {
+        console.log('type ', type, ' wire', wire);
+        switch (type) {
+          case 'БМРЗ 150':
+           return addBMRZ(10, 10);
+          case 'GL 100':
+          case 'CISCO 1000':
+            return addCommutator(10, 10);
+          case 'Медный кабель':
+          case 'Витая пара RJ45':
+            return setWire(type);
+          default:
+            return;
+        }
+      }}/>
       <div className="canvas" ref={canvas} />
-    </div>
+    </Fragment>
   );
 };
 
