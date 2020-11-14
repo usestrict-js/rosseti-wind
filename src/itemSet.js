4e242fc57219502313a81479ba2ebc5263f654d7
Rozhin Alex <alexey.rozhin90@gmail.com>
2020-11-14 15:05:18 +0300

    test

--- src/itemSet.js
+++ src/itemSet.js
@@ -1,66 +1,43 @@
 import React from "react";
-import {Button, ButtonGroup, Card, Elevation, FormGroup, InputGroup} from "@blueprintjs/core";
+import {
+  Button,
+  Card,
+  Elevation,
+  FormGroup,
+  InputGroup,
+} from "@blueprintjs/core";
 
-export const ItemSet = () => <Card interactive={true} elevation={Elevation.TWO}>
-
-        <h2>IED1</h2>
-        <FormGroup inline
-            label="Имя GCB"
-            labelFor="id_gcb-input"
-        >
-            <InputGroup id="id_gcb-input" />
-        </FormGroup>
-
-        <FormGroup inline
-               label="GOOSE ID"
-               labelFor="id_goose-input"
-        >
-        <InputGroup id="id_goose-input"/>
-        </FormGroup>
-
-    <FormGroup inline
-               label="MAC адрес"
-               labelFor="id_mac-input"
-    >
-        <InputGroup id="id_mac-input"/>
+export const ItemSet = () => (
+  <Card interactive={true} elevation={Elevation.TWO}>
+    <h2>IED1</h2>
+    <FormGroup inline label="Имя GCB" labelFor="id_gcb-input">
+      <InputGroup id="id_gcb-input" />
     </FormGroup>
 
-    <FormGroup inline
-               label="APP ID"
-               labelFor="id_app-input"
-    >
-        <InputGroup id="id_app-input"/>
+    <FormGroup inline label="GOOSE ID" labelFor="id_goose-input">
+      <InputGroup id="id_goose-input" />
     </FormGroup>
 
-    <FormGroup inline
-               label="VLAN ID"
-               labelFor="id_vlan-input"
-    >
-        <InputGroup id="id_vlan-input"/>
+    <FormGroup inline label="MAC адрес" labelFor="id_mac-input">
+      <InputGroup id="id_mac-input" />
     </FormGroup>
 
-    <FormGroup inline
-               label="Min Time"
-               labelFor="id_minTime-input"
-    >
-        <InputGroup id="id_minTime-input"/>
+    <FormGroup inline label="APP ID" labelFor="id_app-input">
+      <InputGroup id="id_app-input" />
     </FormGroup>
 
-    <FormGroup inline
-               label="Max Time"
-               labelFor="id_maxTime-input"
-    >
-        <InputGroup id="id_maxTime-input"/>
+    <FormGroup inline label="VLAN ID" labelFor="id_vlan-input">
+      <InputGroup id="id_vlan-input" />
     </FormGroup>
 
+    <FormGroup inline label="Min Time" labelFor="id_minTime-input">
+      <InputGroup id="id_minTime-input" />
+    </FormGroup>
 
+    <FormGroup inline label="Max Time" labelFor="id_maxTime-input">
+      <InputGroup id="id_maxTime-input" />
+    </FormGroup>
 
-    <Button icon="saved">
-        СОХРАНИТЬ
-    </Button>
-
-
-</Card>
-
-
-
+    <Button icon="saved">СОХРАНИТЬ</Button>
+  </Card>
+);
--- src/test/index.js
+++ src/test/index.js
@@ -1,30 +1,46 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import Question from "./question";
 import data from "./questions.json";
 import { Button, Intent } from "@blueprintjs/core";
 import { Link } from "react-router-dom";
 
-const Test = () => (
-  <Fragment>
-    <Link to="/emulator">
-      <Button
-        intent={Intent.SUCCESS}
-        style={{
-          fontSize: "18px",
-          position: "fixed",
-          top: "20px",
-          left: "20px",
-        }}
-      >
-        Эмулятор
-      </Button>
-    </Link>
-    <div>
-      {data.map(({ id, question, variants }) => {
-        return <Question variants={variants} text={question} id={id} />;
-      })}
-    </div>
-  </Fragment>
-);
+const Test = () => {
+  const [answers, setAnswers] = useState([data.map(() => undefined)]);
+  return (
+    <Fragment>
+      <Link to="/emulator">
+        <Button
+          intent={Intent.SUCCESS}
+          style={{
+            fontSize: "18px",
+            position: "fixed",
+            top: "20px",
+            left: "20px",
+          }}
+        >
+          Эмулятор
+        </Button>
+      </Link>
+      <div>
+        {data.map(({ id, question, variants }) => {
+            debugger;
+          return (
+            <Question
+              variants={variants}
+              text={question}
+              id={id}
+              selectedValue={answers[id - 1]}
+              onChange={(event) => {
+                answers[id - 1] = event.target.value;
+                console.log("answers ", answers);
+                setAnswers(answers);
+              }}
+            />
+          );
+        })}
+      </div>
+    </Fragment>
+  );
+};
 
 export default Test;
