67a136d948e6013ddf52cf5815953b06e875202f
Rozhin Alex <alexey.rozhin90@gmail.com>
2020-11-15 03:46:18 +0300

    final: Improve styles

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,31 +1,41 @@
 import React from "react";
-import { Button, Card, FormGroup, InputGroup } from "@blueprintjs/core";
-import { Link } from "react-router-dom";
+import { Button, Card, FormGroup, InputGroup, Intent } from "@blueprintjs/core";
+import { useHistory } from "react-router-dom";
 
-const Auth = () => (
-  <Card style={{ maxWidth: "400px" }}>
-    <FormGroup
-      helperText="Введите Ваши Фамилию Имя Отчество полностью через пробел"
-      label="ФИО"
-      labelFor="text-input"
-      labelInfo="(кириллицей)"
-    >
-      <InputGroup large id="fio-input" placeholder="Фамилия Имя Отчество" />
-    </FormGroup>
+const Auth = () => {
+  const history = useHistory();
+  return (
+    <Card style={{ maxWidth: "400px", margin: '200px' }}>
+      <FormGroup
+        helperText="Введите Ваши Фамилию Имя Отчество полностью через пробел"
+        label="ФИО"
+        labelFor="text-input"
+        labelInfo="(кириллицей)"
+      >
+        <InputGroup large id="fio-input" placeholder="Фамилия Имя Отчество" />
+      </FormGroup>
 
-    <FormGroup
-      helperText="Введите Ваши Фамилию Имя Отчество полностью через пробел"
-      label="№ учебной группы"
-      labelFor="text-input"
-      labelInfo="(только номер)"
-    >
-      <InputGroup large id="group-number" placeholder="номер Вашей группы" />
-    </FormGroup>
+      <FormGroup
+        helperText="Введите Ваши Фамилию Имя Отчество полностью через пробел"
+        label="№ учебной группы"
+        labelFor="text-input"
+        labelInfo="(только номер)"
+      >
+        <InputGroup large id="group-number" placeholder="номер Вашей группы" />
+      </FormGroup>
 
-    <Button large icon="key-enter">
-      <Link to="/tasks">Войти</Link>
-    </Button>
-  </Card>
-);
+      <Button
+        large
+        icon="key-enter"
+        intent={Intent.SUCCESS}
+        onClick={() => {
+          history.push("/emulator");
+        }}
+      >
+        Войти
+      </Button>
+    </Card>
+  );
+};
 
 export default Auth;
